96500eaa59157b0c16513803284622fa855dc6b2
Mohammad Tauheed Raza Patel <80320712+mohammadTauheedrazapatel30@users.noreply.github.com>
2024-09-10 16:53:04 +0530

    Updates done on 10/09

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,12 +7,35 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-   
-  @Input() matDatepicker : any = '';
+  // A formData Property for our forms 
+  // Now we will bind this to our form using ngModel , it's done
+  formData = {
+     fname : "",
+     lname : "",
+     email : "",
+     password : "",
+     city : "",
+     state : "",
+     zipcode : ""
+  }
   
-  constructor() { }
+  constructor(){
+   }
 
   ngOnInit(): void {
+  }
+
+  // here i have created a method for saving the values so
+  onRegister() {
+        // as this condition got true therefore
+        if(this.onValid()){
+            console.log("Form Submitted", this.formData);
+            alert("Registered Successfully !");
+        }    
+  }
+
+  onValid(){
+    return this.formData.fname && this.formData.lname && this.formData.email && this.formData.password && this.formData.city && this.formData.state && this.formData.zipcode;
   }
 
 }
